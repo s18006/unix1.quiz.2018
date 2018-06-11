8b9108c5faa690cdfcbeaf6a687cc9f043ed1781
Kerekes Csaba <s18006@std.it-college.ac.jp>
2018-06-11 01:24:30 +0000

    birthday_question_unix

---
+++ 0611/happy_birthday.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "please enter your next birthday!"
+echo -n year_yyyy
+read year_yyyy
+echo -n month_mm
+read month_mm
+echo -n day_dd
+read day_dd
+
+days=0
+while [ "$d" != "$year_yyyy-$month_mm-$day_dd" ]; do
+    ((i++))
+    d=$(date +%F -d "$d 1 day")
+done
+
+echo $i "day(s) remaining"
+
