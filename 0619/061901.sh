1162e3f7e21d12869fcbbdf1816ead63ffef6e46
Kerekes Csaba <s18006@std.it-college.ac.jp>
2018-06-19 08:00:51 +0000

    change kadai0619

--- 0619/061901.sh
+++ 0619/061901.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-for i in $(seq 1 11);
+
+for i in $(seq 0 10);
 do
-    class=$(cat age | sort -n | uniq -c | head -n $((i*10)) | tail -10 | awk -F, '{sum += $1} END{print sum}')
-    echo $"number of $(((i*10)-10))-$(((i*10-1))) ages $class"
+    if [ $i = 0 ]; then
+        class1=$(cat age | sort -n | uniq -c | grep ' [0-9]$'| awk '{print $1}' | awk -F, '{sum += $1} END{print sum}')
+        echo $"number of 0-9 ages $class1"
+    fi
+    if [ $i -ge 1 ]; then
+        class2=$(cat age | sort -n | uniq -c | grep ' '$i'[0-9]$'| awk '{print $1}' | awk -F, '{sum += $1} END{print sum}')
+        echo $"number of $(((i*10)))-$(((i*10+9))) ages $class2"
+    fi
 done
 
 all=$(cat age | sort -n | uniq -c | awk -F, '{sum += $1} END{print sum}')
 echo $"number of all is $all"
+
+                                                                                                                                       
